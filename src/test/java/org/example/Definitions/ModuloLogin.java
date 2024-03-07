84c1a5534a1d0c31ca1b243e22c7771e28ba5f9a
Tere c <t.sandovalsaldia@gmail.com>
2024-03-06 21:24:43 -0300

    Se eliminan archivos de proyecto, se deja solo login

--- src/test/java/org/example/Definitions/ModuloLogin.java
+++ src/test/java/org/example/Definitions/ModuloLogin.java
@@ -6,23 +6,4 @@ import org.example.PageObjects.ModuloLoginPO;
 
 public class ModuloLogin {
     private ModuloLoginPO moduloLoginPO = new ModuloLoginPO();
-
-    @When("Ingresa sus credenciales y inicia sesión")
-    public void ingresaSusCredencialesYEIniciaSesión() {
-        moduloLoginPO.ingresarUsuario();
-        moduloLoginPO.ingresarContraseña();
-        moduloLoginPO.hacerClicEnIniciarSesión();
-    }
-
-    @When("Ingresa sus credenciales incorrectas")
-    public void ingresaSusCredencialesIncorrectas() {
-        moduloLoginPO.ingresarUsuarioIncorrecto();
-        moduloLoginPO.ingresarContraseña();
-        moduloLoginPO.hacerClicEnIniciarSesión();
-    }
-
-    @Then("Valido que se despliegue el mensaje de error")
-    public void validoQueSeDespliegueElMensajeDeError() {
-        moduloLoginPO.validarMensajeError();
-    }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -4,9 +4,3 @@ Feature: Inicio de sesión en el sitio web
     Given accedo a la página de inicio del sitio web
 
   Scenario: Iniciar sesión con usuario correcto
-    When Ingresa sus credenciales y inicia sesión
-    Then Valido que se despliegue el dashboard del usuario
-
-  Scenario: Iniciar sesión con usuario incorrecto
-    When Ingresa sus credenciales incorrectas
-    Then Valido que se despliegue el mensaje de error
